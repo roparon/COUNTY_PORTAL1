7a1f4f4c1c91765e6a2e069d78c5a7b3d6d6b4d4
Aron Rop <aaronrop40@gmail.com>
2025-08-26 21:22:04 +0300

    modify my run.py to use wsg

--- run.py
+++ run.py
@@ -5,5 +5,13 @@ from app import create_app
 app = create_app()
 
 
+if __name__ == "__main__":import os
+from app import create_app
+
+app = create_app()
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
     app.run(debug=True)
