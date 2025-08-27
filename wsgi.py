28a6944b1d4732f7bf9499f39fcb0e4082a70227
Aron Rop <aaronrop40@gmail.com>
2025-08-27 19:53:42 +0300

    modified the csrf token

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
-from app import create_app
-
-
-
-app = create_app()
-
-
-if __name__ == "__main__":import os
+import os
 from app import create_app
 
 app = create_app()
@@ -13,5 +6,3 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-
-    app.run(debug=True)
